7ec5c38bb1a20b25b50a7f0c75d32c60923bb343
chris@box <cmwhee@gmail.com>
2017-12-05 10:23:58 -0500

    initial commit

---
+++ EmpyrionAPIMessageBroker.cs
@@ -0,0 +1,96 @@
+﻿using Eleon.Modding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace EmpyrionAPIMessageBroker
+{
+    public class MessageBroker
+    {
+        public ModGameAPI GameAPI;
+        private Dictionary<ushort, Action<CmdId, object>> actionTracker = new Dictionary<ushort, Action<CmdId, object>>();
+        private static ushort handledMessageCount = 0;
+        private static ushort offset = 1000;
+
+        private bool verbose;
+
+        public MessageBroker(ModGameAPI dediAPI, bool verbose=false)
+        {
+            this.GameAPI = dediAPI;
+            this.verbose = verbose;
+        }
+
+        private void defaultHandler(CmdId cmd, object any) { }
+
+
+        public void ExecuteCommand(APICmd cmd)
+        {
+            this.ExecuteCommand<Object>(cmd, defaultHandler);
+        }
+
+        public void ExecuteCommand<ResponseType>(APICmd cmd, Action<ResponseType> handler)
+        {
+            Action<CmdId, object> outerHandler = (x, y) => handler((ResponseType)y);
+            trackHandler(cmd, outerHandler);
+        }
+
+        public void ExecuteCommand<ResponseType>(APICmd cmd, Action<CmdId, ResponseType> handler)
+        {
+            Action<CmdId, object> outerHandler = (x, y) => handler(x, (ResponseType)y);
+            trackHandler(cmd, outerHandler);
+        }
+
+        private void trackHandler(APICmd cmd, Action<CmdId, object> handler)
+        {
+            var seqNr = (ushort)((handledMessageCount++ % 30000) + offset);
+            actionTracker[seqNr] = handler;
+            GameAPI.Game_Request(cmd.cmd, seqNr, cmd.data);
+        }
+
+        public void HandleMessage(CmdId eventId, ushort seqNr, object data)
+        {
+            if (!actionTracker.ContainsKey(seqNr)) return;
+            var action = actionTracker[seqNr];
+            action(eventId, data);
+            deprovisionSequenceNumber(seqNr);
+
+        }
+
+        private void log(Func<string> logMsg)
+        {
+            if (this.verbose)
+            {
+                GameAPI.Console_Write(logMsg());
+            }
+        }
+
+        public void deprovisionSequenceNumber(ushort seqnr)
+        {
+            actionTracker.Remove(seqnr);
+        }
+    }
+
+    public class APICmd
+    {
+        public CmdId cmd { get; }
+        public object data { get; }
+
+        public APICmd(CmdId cmd, object data = null)
+        {
+            this.cmd = cmd;
+            this.data = data;
+        }
+
+        public static APICmd operator +(APICmd cmd, object data)
+        {
+            return new APICmd(cmd.cmd, data);
+        }
+
+        public static implicit operator APICmd(CmdId d)
+        {
+            return new APICmd(d);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Empyrion API Message Broker
+
+## FAQ
+
+### Q:  What is this?
+
+The Empyrion mod api is kind of unintuitive and difficult to work with.  In the course of developing a few mods, I developed an understanding of what was available, and how to make it easier to work with.  I developed this library to enable callback-style management of API requests and responses.
+
+### Q:  How do I use it?
